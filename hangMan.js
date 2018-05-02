8ccd942b2d54a2fcbf97b23d048df97979c5bf97
OJPARKINSON <olliepar01@gmail.com>
2018-05-02 21:16:01 +0100

    hit minimum requirement
    work on extra features and interdace now

--- hangMan.js
+++ hangMan.js
@@ -1,37 +1,55 @@
 let guess = {};
 let wordSplit = {};
-let incorrectLetters = {};
-incorrectLetters.new += "Incorrect letters that have been guessed: "
-incorrectLetters.display = word;
+let LettersGuessed = {};
+LettersGuessed.new += "Letters that have been guessed: "
 let display = {};
+
+
 window.onload = function start() {
 	word = prompt("Please enter a word for player two to guess: ");
+
 	display.new = "_".repeat(word.length).split("");
 	document.getElementById("wordDisplay").innerHTML = display.new;
-	document.getElementById("incorrectLetters").innerHTML = incorrectLetters.new;
+	document.getElementById("incorrectLetters").innerHTML = LettersGuessed.new;
 	wordSplit.new = word.split("");
 	console.log(wordSplit.new);
 	displayUpdate();
 }
+
+function anotherGo() {
+	word = "";
+	word = prompt("Please enter a word for player two to guess: ");
+	display.new = "_".repeat(word.length).split("");
+	document.getElementById("wordDisplay").innerHTML = display.new;
+	document.getElementById("incorrectLetters").innerHTML = LettersGuessed.new;
+	wordSplit.new = word.split("");
+	console.log(wordSplit.new);
+	displayUpdate();
+}
+
+LettersGuessed.display = "";
 function displayUpdate() {
 	document.getElementById("wordDisplay").innerHTML = (display.new);
-		document.getElementById("incorrectLetters").innerHTML = incorrectLetters.new;
+	document.getElementById("incorrectLetters").innerHTML = LettersGuessed.new;
 }
+
 function guessing() {
-	guess.new = document.getElementById("TbxGuess").value;}
+	guess.new = document.getElementById("TbxGuess").value;
+}
+
 function check() {
 	for (count = 0; count <= (display.new.length); count++){
+
 		if (wordSplit.new[count] == guess.new) {
 			console.log(guess.new, " is correct");
 			display.new.splice(count, 1, guess.new);
-			displayUpdate();
 		} else {
 			console.log("Incorrect");
-			if (incorrectLetters.display.indexOf(guess.new) == -1) {
-				incorrectLetters.display += guess.new;
-				incorrectLetters.new += guess.new + ", ";
+			if (guess.new != wordSplit.new[count] && LettersGuessed.display.indexOf(guess.new) === -1) {
+				LettersGuessed.display += guess.new;
+				LettersGuessed.new += guess.new + ", ";
 			}
-			displayUpdate();
 		}
+		displayUpdate()
 	}
 }
