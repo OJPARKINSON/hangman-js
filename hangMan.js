7746191a8e3bebaa2e564df0315421403b2958b3
OJPARKINSON <olliepar01@gmail.com>
2018-05-02 16:10:31 +0100

    add stylesheet

--- hangMan.js
+++ hangMan.js
@@ -1,18 +1,19 @@
-var guess = {};
+let guess = {};
 let wordSplit = {};
-let wordLenth = {};
+let incorrectLetters = {};
 let display = {};
 window.onload = function start() {
 	word = prompt("Please enter a word for player two to guess: ");
 	display.new = "_".repeat(word.length).split("");
 	document.getElementById("wordDisplay").innerHTML = display.new;
-	document.getElementById("inccorectLetters").innerHTML = wordLenth.new;
+	document.getElementById("incorrectLetters").innerHTML = incorrectLetters.new;
 	wordSplit.new = word.split("");
 	console.log(wordSplit.new);
 	displayUpdate();
 }
 function displayUpdate() {
 	document.getElementById("wordDisplay").innerHTML = (display.new);
+		document.getElementById("incorrectLetters").innerHTML = incorrectLetters.new;
 }
 function guessing() {
 	guess.new = document.getElementById("TbxGuess").value;}
@@ -24,6 +25,8 @@ function check() {
 			displayUpdate();
 		} else {
 			console.log("Incorrect");
+			incorrectLetters.new += guess.new;
+			displayUpdate();
 		}
 	}
 }
