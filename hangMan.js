40ac060e2c4ef48dc48fd35936efc040776c4f8b
OJPARKINSON <olliepar01@gmail.com>
2018-05-02 13:11:49 +0100

    The guessing and display now works

--- hangMan.js
+++ hangMan.js
@@ -5,39 +5,24 @@ let display = {};
 window.onload = function start() {
 	word = prompt("Please enter a word for player two to guess: ");
 	display.new = "_".repeat(word.length).split("");
-	console.log(display.new);
 	document.getElementById("wordDisplay").innerHTML = display.new;
 	document.getElementById("inccorectLetters").innerHTML = wordLenth.new;
-	wordLenth.new = "";
 	wordSplit.new = word.split("");
 	console.log(wordSplit.new);
 	displayUpdate();
 }
 function displayUpdate() {
-	document.getElementById("wordDisplay").innerHTML = (display.new)
+	document.getElementById("wordDisplay").innerHTML = (display.new);
 }
 function guessing() {
-	guess.new = document.getElementById("TbxGuess").value;
-	//console.log(wordSplit.new);
-}
+	guess.new = document.getElementById("TbxGuess").value;}
 function check() {
-	let count = 0;
-	for (count = 0; count <= (display.new.length); count++) {
-		console.log(wordSplit.new.slice(count));
-		console.log(guess.new);
-		if (wordSplit.new.slice(count) == guess.new) {
+	for (count = 0; count <= (display.new.length); count++){
+		if (wordSplit.new[count] == guess.new) {
 			console.log(guess.new, " is correct");
-			display.new.splice(count, 1, guess.new); // append *****
-		//	console.log(wordSplit.new.indexOf(guess.new));
-		//	console.log(guess.new);
+			display.new.splice(count, 1, guess.new);
 			displayUpdate();
-		} //else if () {
-		//	console.log(guess.new, " is correct");
-		//	console.log(wordSplit.new.indexOf(guess.new));
-		//	display.new.splice(count, 1, guess.new);s
-		//	console.log(count);
-		//	displayUpdate(); }
-	else {
+		} else {
 			console.log("Incorrect");
 		}
 	}
