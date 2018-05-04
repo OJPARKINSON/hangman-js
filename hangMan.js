2183bd582294b60e71c7ad668e5717d0642ca66f
OJPARKINSON <olliepar01@gmail.com>
2018-05-05 00:54:34 +0100

    respose display ✅

--- hangMan.js
+++ hangMan.js
@@ -1,55 +1,56 @@
-let guess = {};
-let wordSplit = {};
+let guess = "";
+let wordSplit = [];
 let LettersGuessed = {};
-LettersGuessed.new += "Letters that have been guessed: "
-let display = {};
-
+let display = [];
+let correctLetters = [];
+let word = "";
 
 window.onload = function start() {
 	word = prompt("Please enter a word for player two to guess: ");
-
-	display.new = "_".repeat(word.length).split("");
-	document.getElementById("wordDisplay").innerHTML = display.new;
+	display = "_".repeat(word.length).split("");
+	document.getElementById("wordDisplay").innerHTML = display;
 	document.getElementById("incorrectLetters").innerHTML = LettersGuessed.new;
-	wordSplit.new = word.split("");
-	console.log(wordSplit.new);
+	wordSplit = word.split("");
 	displayUpdate();
 }
 
 function anotherGo() {
-	word = "";
+	word = ""; //resets the word for multiple goes
 	word = prompt("Please enter a word for player two to guess: ");
-	display.new = "_".repeat(word.length).split("");
-	document.getElementById("wordDisplay").innerHTML = display.new;
+	display = "_".repeat(word).split("");
+	document.getElementById("wordDisplay").innerHTML = display;
 	document.getElementById("incorrectLetters").innerHTML = LettersGuessed.new;
-	wordSplit.new = word.split("");
-	console.log(wordSplit.new);
+	wordSplit = word.split("");
 	displayUpdate();
 }
-
-LettersGuessed.display = "";
+LettersGuessed.new = "Letters that have been guessed: "
+LettersGuessed.display = " ";
 function displayUpdate() {
-	document.getElementById("wordDisplay").innerHTML = (display.new);
-	document.getElementById("incorrectLetters").innerHTML = LettersGuessed.new;
+	document.getElementById("wordDisplay").innerHTML = (display);
+	document.getElementById("incorrectLetters").innerHTML = (LettersGuessed.new);
 }
 
 function guessing() {
-	guess.new = document.getElementById("TbxGuess").value;
+	guess = document.getElementById("TbxGuess").value;
+}
+
+function singleGuess() {
+	for (count = 0; count <= (display.length); count++){
+		if (wordSplit[count] == guess) {
+			display.splice(count, 1, guess);
+			correctLetters++;
+		} else {
+			if (guess != wordSplit[count] && LettersGuessed.display.indexOf(guess) === -1) {
+				LettersGuessed.display += guess;
+				LettersGuessed.new += guess + ", ";
+			}
+		}
+	}displayUpdate()
+	 check()
 }
 
 function check() {
-	for (count = 0; count <= (display.new.length); count++){
-
-		if (wordSplit.new[count] == guess.new) {
-			console.log(guess.new, " is correct");
-			display.new.splice(count, 1, guess.new);
-		} else {
-			console.log("Incorrect");
-			if (guess.new != wordSplit.new[count] && LettersGuessed.display.indexOf(guess.new) === -1) {
-				LettersGuessed.display += guess.new;
-				LettersGuessed.new += guess.new + ", ";
-			}
-		}
-		displayUpdate()
+	if (correctLetters === word.length) {
+		console.log("well done");
 	}
 }
