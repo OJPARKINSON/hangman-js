e45809f20a8f9e6803a29c088b4780c8ec29490d
OJPARKINSON <olliepar01@gmail.com>
2018-05-05 14:07:26 +0100

    bug fixes

--- hangMan.js
+++ hangMan.js
@@ -1,17 +1,16 @@
-let guess = "";
+var guess, word;
 let wordSplit = [];
 let LettersGuessed = {};
 let display = [];
 let correctLetters = [];
-let word = "";
 let goAgain = document.getElementById("BtnAgain");
 
 window.onload = function start() {
 	word = prompt("Please enter a word for player two to guess: ");
-	display = "_".repeat(word.length).split("");
-	document.getElementById("wordDisplay").innerHTML = display;
-	document.getElementById("incorrectLetters").innerHTML = LettersGuessed.new;
+	document.getElementById("wordDisplay").innerHTML = (display = "_".repeat(word.length).split(""));
+	document.getElementById("incorrectLetters").innerHTML = (LettersGuessed.new = "Letters that have been guessed: ");
 	wordSplit = word.split("");
+	LettersGuessed.display = " ";
 	goAgain.disabled = true;
 	displayUpdate();
 }
@@ -21,19 +20,16 @@ function anotherGo() {
 	display -= display;
 	wordSplit -= wordSplit;
 	correctLetters -= correctLetters;
-	LettersGuessed.new = "Letters that have been guessed: ";
 	LettersGuessed.display = " ";
 	word = prompt("Please enter a word for player two to guess: ");
-	display = "_".repeat(word.length).split("");
-	document.getElementById("wordDisplay").innerHTML = display;
+	document.getElementById("wordDisplay").innerHTML = (display = "_".repeat(word.length).split(""));
 	document.getElementById("incorrectLetters").innerHTML = LettersGuessed.new;
 	wordSplit = word.split("");
 	goAgain.disabled = true;
 	document.getElementById("interactiveColor").style.background = "#d12141"
 	displayUpdate();
 }
-LettersGuessed.new = "Letters that have been guessed: "
-LettersGuessed.display = " ";
+
 function displayUpdate() {
 	document.getElementById("wordDisplay").innerHTML = (display);
 	document.getElementById("incorrectLetters").innerHTML = (LettersGuessed.new);
@@ -41,9 +37,11 @@ function displayUpdate() {
 
 function guessing() {
 	guess = document.getElementById("TbxGuess").value;
+	document.getElementById("TbxGuess").value = "";
 }
 
 function singleGuess() {
+	console.log(LettersGuessed.display.indexOf(guess));
 	for (count = 0; count <= (display.length); count++){
 		if (wordSplit[count] == guess) {
 			display.splice(count, 1, guess);
@@ -61,10 +59,7 @@ function singleGuess() {
 
 function check() {
 	if (correctLetters === word.length) {
-		console.log("well done");
 		goAgain.disabled = false;
 		document.getElementById("interactiveColor").style.background = "green";
-	} else {
-		console.log("keep going");
 	}
 }
