2550282074393b83d6ac60b5f9cbe1cf22ec1562
OJPARKINSON <olliepar01@gmail.com>
2018-07-19 02:37:23 +0100

    lots of design added
    the image of hangman needs working on and needs finnishing

--- hangMan.js
+++ hangMan.js
@@ -5,7 +5,11 @@ let display = [];
 let correctLetters = [];
 let goAgain = document.getElementById("BtnAgain");
 
-window.onload = function start() {
+function start() {
+	word -= word; //resets the word for multiple goes
+	display -= display;
+	wordSplit -= wordSplit;
+	correctLetters -= correctLetters;
 	word = prompt("Please enter a word for player two to guess: ");
 	setup()
 }
@@ -16,21 +20,6 @@ function setup() {
 	wordSplit = word.split("");
 	LettersGuessed.display = " ";
 	goAgain.disabled = true;
-	displayUpdate();
-}
-
-function anotherGo() {
-	word -= word; //resets the word for multiple goes
-	display -= display;
-	wordSplit -= wordSplit;
-	correctLetters -= correctLetters;
-	LettersGuessed.display = " ";
-	word = prompt("Please enter a word for player two to guess: ");
-	document.getElementById("wordDisplay").innerHTML = (display = "_".repeat(word.length).split(""));
-	document.getElementById("incorrectLetters").innerHTML = LettersGuessed.new;
-	wordSplit = word.split("");
-	goAgain.disabled = true;
-	document.getElementById("interactiveColor").style.background = "#d12141";
 	displayUpdate();
 }
 
@@ -45,22 +34,26 @@ function guessing() {
 }
 
 function singleGuess() {
-	console.log(LettersGuessed.display.indexOf(guess));
 	for (count = 0; count <= (display.length); count++){
 		if (wordSplit[count] == guess) {
 			display.splice(count, 1, guess);
-			document.getElementById("interactiveColor").style.background = "green"; //Changes the background colour if correct
 			correctLetters++;
-		} else {
-			if (guess != wordSplit[count] && LettersGuessed.display.indexOf(guess) === -1) {
-				document.getElementById("interactiveColor").style.background = "red"; 
+			document.getElementById("interactiveColor").style.background = "green"; //Changes the background colour if correct
+		} else if (guess != wordSplit[count] && LettersGuessed.display.indexOf(guess) == -1) {
 				LettersGuessed.display += guess;
 				LettersGuessed.new += guess + ", ";
-			}
+				document.getElementById("interactiveColor").style.background = "#b14c5a";
+		}else if (correctLetters == 1){
+				document.getElementById("interactiveColor").style.background = "green"; //making sure the background on the first correct guess goes green
 		}
 	}
 	displayUpdate()
+	displayimg()
 	if (correctLetters === word.length) {
 		goAgain.disabled = false; //allows the button to be pressed
 	}
+}
+function displayimg() {
+	var img = ["hang1.png", "hang2.png", "hang3.png", "hang4.png", "hang5.png", "hang6.png", "hang7.png", "hang8.png", "hang9.png", "hang10.png", "hang11.png"]
+	document.getElementById('hangman').src = img[10];
 }
